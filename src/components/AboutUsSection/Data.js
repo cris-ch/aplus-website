53578d756cd5036ad452b91365a1de8a36fd3428
Cristian <cristian.christensen@gmail.com>
2022-09-13 12:22:15 +0200

    Added About Us Section.

---
+++ src/components/AboutUsSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: true,
+  topLine: "Academia de Inglés",
+  headline: "Aprende Inglés con nosotros",
+  description: "Somos una academia de inglés con más de 20 años de experiencia. Nuestro método de enseñanza es único y garantiza resultados. ¡Aprende inglés con nosotros!",
+  buttonLabel: "Contacta con nosotros",
+  imgStart: false,
+  img: require("../../images/svg-1.svg").default,
+  alt: "Books",
+  dark: true,
+  primary: true,
+  darkText: true,
+};
+
